b0c925114d8b19fbf6ab3b17f6aeaf8083d95f1f
8jsalamone <31480869+8jsalamone@users.noreply.github.com>
2017-08-30 10:03:24 -0500

    Joe is Cool

---
+++ joeiscool.js
@@ -0,0 +1,9 @@
+function collegeapplication(){
+  if (ACT > 29 && "stink" = false) {
+    console.log("Welcome to Northwestern!")
+  }
+  if (ACT <= 29 || stink=true){
+    console.log("Sorry bud, try DePaul")
+}
+  }
+  collegeapplication(34 && stink=true)
