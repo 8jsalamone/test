16ab59bec816974b09985dfbea837cdac8ff2fca
8jsalamone <31480869+8jsalamone@users.noreply.github.com>
2017-08-31 08:44:10 -0500

    Delete joeiscool.js

--- joeiscool.js
+++
@@ -1,9 +0,0 @@
-function collegeapplication(){
-  if (ACT > 29 && "stink" = false) {
-    console.log("Welcome to Northwestern!")
-  }
-  if (ACT <= 29 || stink=true){
-    console.log("Sorry bud, try DePaul")
-}
-  }
-  collegeapplication(34 && stink=true)
